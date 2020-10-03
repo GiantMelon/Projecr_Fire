42eb3dd51a40706ecc0432550319e3c64d2f3caf
GiantMelon <rhysllew1999@gmail.com>
2020-10-03 23:53:12 +0100

    REEEEEE

---
+++ Assets/Scripts/HEXAGONS.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public static class HexMetrics
+{
+
+    public const float outerRadius = 10f;
+
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+    };
+}
+
+
+public class HEXAGONS : MonoBehaviour
+{
+
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+    HexCell[] cells;
+
+    void Awake()
+    {
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = x * 10f;
+        position.y = 0f;
+        position.z = z * 10f;
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
